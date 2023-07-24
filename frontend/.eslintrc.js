363a397b1059d8b462db3f213d7d31e0175f9fae
44nash <44mcode@gmail.com>
2023-07-23 21:42:37 -0400

    test commit

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,28 +1,26 @@
 module.exports = {
   ignorePatterns: [
-    'build', // build folder is for releases
+    "build", // build folder is for releases
   ],
   root: true,
   env: {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ["plugin:react/recommended", "airbnb"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 13,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ["react"],
   rules: {
-    'no-console': 'off',
-    'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
+    "no-console": "off",
+    "linebreak-style": [
+      "error",
+      process.platform === "win32" ? "windows" : "unix",
+    ],
   },
 };
